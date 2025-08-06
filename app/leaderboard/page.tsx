3ace4db809b1fcce9837040bc933be91fed65e62
Jason <jtoy@jtoy.net>
2025-08-05 19:07:39 -0700

    updated

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { ChevronDown, Trophy, TrendingUp, TrendingDown } from 'lucide-react'
 
 export default function LeaderboardPage() {
